58c3ed2e29e28fbcffe6775a945cbfa88a9ee603
narsingh24 <narsingh@iclimbs.com>
2024-12-25 14:30:28 +0530

    Integraion with android

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express")
+const os = require('os');
 const ErrorMiddleware = require("./middleswares/Error.middleware")
 const ErrorHandler = require("./others/ErrorHandler.class")
+// google auth routes
+const authRoute = require("./routes/GoogleAuth.routes")
+// user routes
+const userRouter = require("./routes/UserProfile.routes")
 const app = express()
 app.use(express.json())
 require("dotenv").config()
-const port = process.env.PORT || 5000
+//console.log('process env::::::; ',process.env);
+
 const cors = require("cors")
 app.use(cors())
 
@@ -19,29 +25,39 @@ app.use(passport.initialize())
 passportHandler()
 
 const { hashPassword, comparePassword } = require("./others/Extra.functions")
-// comparePassword("4sep2001", "$2b$10$GEgW0CregNwi95ZTWzWT1.Db9eibuK03AcHtXFB7V54BedAp5SXw2").then(e =>
-//     console.log(e)
-// )
 
 app.get("/", (req, res, next) => {
 
     res.send(`<a href="/auth/google">Login</a>`)
 })
-
-
-// google auth routes
-const authRoute = require("./routes/GoogleAuth.routes")
 app.use('/auth/google', authRoute)
-
-// user routes
-const userRouter = require("./routes/UserProfile.routes")
 app.use('/api/user', userRouter)
 
 
 
 
+
+// Function to get local IP address (non-127.0.0.1 IP)
+function getLocalIPAddress() {
+    const interfaces = os.networkInterfaces();
+    for (let interfaceName in interfaces) {
+        for (let iface of interfaces[interfaceName]) {
+            // Check if the interface is an IPv4 address and not a loopback
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '127.0.0.1'; // Fallback to localhost
+}
+
+// Get the local IP address
+const localIP = getLocalIPAddress();
+const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log(port);
+     // Complete URL (protocol + IP address + port)
+     const completeURL = `http://${localIP}:${port}`;
+     console.log('Complete url: ', completeURL);
 })
 
 app.use(ErrorMiddleware)
--- routes/GoogleAuth.routes.js
+++ routes/GoogleAuth.routes.js
@@ -3,17 +3,14 @@ const passport = require("passport");
 const user = require("../models/User.models");
 // const isAuthenticated = require("../middlewares/isAuthenticated");
 // const verifyToken = require("../middlewares/verifyJWT.middlewares");
-
 // const isAuthenticated = require("../middlewares/isAuthenticated");
 const authRoute = express.Router()
-
 
 // google login
 authRoute.route('/').get(passport.authenticate('google', { scope: ["profile", "email"], session: false }));
 
 
 authRoute.route('/callback').get(passport.authenticate('google', { failureRedirect: '/', session: false }), (req, res) => {
-    // res.send("done")
     res.json({ user: req.user, success: true, token: req?.user?.token }); // Send token in the response
 })
 
